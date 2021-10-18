64e9eee328f9321fc5e6913fb866f0de02a33238
SHIVANSH CHOUDHARY <shivansh20127@iiitd.ac.in>
2021-10-19 00:36:28 +0530

    This is reduntant file.

--- Assignment_2/grade.java
+++ Assignment_2/grade.java
@@ -3,17 +3,5 @@ package com.company;
 
 public class grade
 {
-    private final Data graded_by;
-    private final int graded_marks;
-    grade(Data _obj,int obtained)
-    {
-        this.graded_by=_obj;
-        this.graded_marks=obtained;
-    }
-    public Data getGraded_by() {
-        return this.graded_by;
-    }
-    public int getGraded_marks() {
-        return this.graded_marks;
-    }
+    
 }
